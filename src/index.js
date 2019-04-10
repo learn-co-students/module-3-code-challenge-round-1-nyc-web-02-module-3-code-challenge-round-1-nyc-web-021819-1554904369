46cc78af650462860178dc2aca5df884827d661c
rrosztoczy <rrosztoczy@gmail.com>
2019-04-10 11:12:29 -0400

    MADE IT NICER TO LOOK AT AHA

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+
 const theatreId = 45;
 const movieUrl = 'https://evening-plateau-54365.herokuapp.com/theatres/45';
 const ticketUrl = 'https://evening-plateau-54365.herokuapp.com/tickets'
@@ -10,67 +11,19 @@ let ticketAdapter = adapter(ticketUrl);
 // DOM Variables
 let moviesContainer = document.querySelector('div.showings');
 
-
-
-
-// capacity: 20
-// film: {title: "If Not Now, When? 2", runtime: 113}
-// id: 479
-// showtime: "02:20AM"
-// tickets_sold: 3
-
 // event listeners
 moviesContainer.addEventListener('click', e => {
-    
+    // Prevent purchase if sold out
     if (e.target.dataset.action === "buy" ) {
-        const movieId = parseInt(e.target.dataset.id)
-        console.log(movieId);
-        // purchase ticket
-        // decrement remaning tickets by one by post
+        const movieId = parseInt(e.target.dataset.id);
+        // use ticket adapter for post
         (async () => {
         const createdTicket = await ticketAdapter.create({showing_id: movieId})
-        console.log(createdTicket);
         })();
         // reload tickets
-       renderTickets();
+       loadMovies();
     }
 });
-
-// Required Keys
-
-// {
-//   showing_id: <add showing_id here>
-// }
-
-// Example Responses:
-
-// Successfully created ticket
-// { "id": 3820, "showing_id": 182, "created_at": "2017-11-13T12:12:28.682Z" }
-// Sold out
-// { "error": "That showing is sold out" }
-// // status 422
-
-
-
-
-
-
-// load initial movie data and render
-async function renderTickets() {
-theater = await movieAdapter.getAll()
-movies = await theater.showings
-console.log(movies)
-moviesContainer.innerHTML = ''
-renderMoviesList(movies)
-}
-renderTickets()
-
-
-
-
-
-
-
 
 // render functions
 function renderMoviesList(movies) {
@@ -110,3 +63,14 @@ function soldOut(movie, remaining_tickets) {
         buttonDiv.parentElement.innerHTML = `
         <div class="ui blue button" "data-tickets=${remaining_tickets} data-id=${movie.id} data-action="sold out">Sold Out</div>`
 }
+
+// load initial movie data and render
+async function loadMovies() {
+    theater = await movieAdapter.getAll()
+    movies = await theater.showings
+    console.log(movies)
+    moviesContainer.innerHTML = ''
+    renderMoviesList(movies)
+}
+
+loadMovies()
